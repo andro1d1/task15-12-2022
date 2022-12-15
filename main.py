9ab1c809b019d4079d4bcbdddb506de371755f7d
Andrey Seresev <aaseresev@miem.hse.ru>
2022-12-15 17:14:14 +0300

    some changes

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from googlewriter import GoogleWriter
 from amoCRM import AmoCrm
-from datetime import date
 
 def get_values(dict: dict)->list:
     """
@@ -12,9 +11,10 @@ def main():
 
     writer = GoogleWriter()
     crm = AmoCrm()
+    
     # events = crm.filter_entity_type("company") # one of [lead, contact, company, customer, task]
     
-    events = crm.filter_date() # input date
+    events = crm.filter_date() # last 12 hours, can change in amoCRM.py
 
     written_data = [[i.account_id, i.id, i.type, i.entity_id, 
                      i.entity_type, i.created_at.strftime("%H-%M-%S %B %d, %Y"), str(i.value_before), str(i.value_after)] for i in events]
